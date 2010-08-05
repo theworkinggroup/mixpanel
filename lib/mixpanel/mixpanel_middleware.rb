2e2b552103741c97fea6a85ef03a295a8d076bfa
Jamie Wong <jamie.lf.wong@gmail.com>
2010-08-05 14:11:31 -0400

    Script tweaks

--- lib/mixpanel/mixpanel_middleware.rb
+++ lib/mixpanel/mixpanel_middleware.rb
@@ -23,7 +23,6 @@ class MixpanelMiddleware
   def update_response!
     @response.each do |part|
       if is_regular_request? && is_html_response?
-        part.gsub!("</head>", "#{render_mixpanel_scripts}</head>")
         part.gsub!("</head>", "#{render_event_tracking_scripts}</head>")
       elsif is_ajax_request? && is_html_response?
         part.gsub!(part, render_event_tracking_scripts + part)
@@ -55,25 +54,6 @@ class MixpanelMiddleware
     @headers["Content-Type"].include?("text/javascript") if @headers.has_key?("Content-Type")
   end
 
-  def render_mixpanel_scripts
-    <<-EOT
-  <script type='text/javascript'>
-    var mp_protocol = (('https:' == document.location.protocol) ? 'https://' : 'http://');
-    document.write(unescape('%3Cscript src="' + mp_protocol + 'api.mixpanel.com/site_media/js/api/mixpanel.js" type="text/javascript"%3E%3C/script%3E'));
-  </script>
-  <script type='text/javascript'>
-    try {
-      var mpmetrics = new MixpanelLib('#{@token}');
-    } catch(err) {
-      null_fn = function () {};
-      var mpmetrics = {
-        track: null_fn,  track_funnel: null_fn,  register: null_fn,  register_once: null_fn, register_funnel: null_fn
-      };
-    }
-  </script>
-    EOT
-  end
-
   def delete_event_queue!
     @env.delete('mixpanel_events')
   end
@@ -86,7 +66,7 @@ class MixpanelMiddleware
   def render_event_tracking_scripts(include_script_tag=true)
     return "" if queue.empty?
 
-    output = queue.map {|event| %(mpmetrics.track("#{event[:event]}", #{event[:properties].to_json});) }.join("\n")
+    output = queue.map {|event| %(Mixpanel.track("#{event[:event]}", #{event[:properties].to_json});) }.join("\n")
 
     output = "try {#{output}} catch(err) {}"
 
